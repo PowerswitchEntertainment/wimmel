2fe27e2c99119e86b8f4753fa444494f737d30e0
Simon Vetter <github@powerswitch-entertainment.de.vu>
2013-04-05 23:51:30 +0200

    * added html file * some kind of documentation

--- web/tile.dart
+++ web/tile.dart
@@ -1,20 +1,36 @@
 part of wimmel;
 
+/**
+ * Tile of the background image
+ * The background image is tiled to prevent timeouts and memory overload and to save render time  
+ */
 class Tile {
   var image = new ImageElement();
   var manager;
   var error=true;
   
+  /**
+   * Resource was successfully loaded
+   */
   void onload(ev) {
     manager.registerFinished();
     error=false;
   }
   
+  /**
+   * Resource couldn't be loaded
+   */
   void onerror(ev) {
     // TODO: create error message
     manager.registerFinished();
   }
   
+  /**
+   * Constructor
+   * @param manager resource manager instance to be notified
+   * @param prefix  tile prefix (folder)
+   * @param part    tile part (number)
+   */
   Tile(manager, prefix, part) {
     this.manager = manager;
     
--- web/wimmelspiel.dart
+++ web/wimmelspiel.dart
@@ -1,3 +1,18 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 library wimmel;
 
 import 'dart:html';
@@ -6,16 +21,21 @@ import 'dart:async';
 import 'dart:math';
 import 'dart:json';
 
-part 'ressource_manager.dart';
+part 'resource_manager.dart';
 part 'tile.dart';
 part 'game.dart';
 part 'camera.dart';
 part 'json.dart';
 
+/**
+ * Main-Function
+ * 
+ * load all game ressources and prepare to launch game 
+ */
 void main() {
-  var manager = new RessourceManager();
-  var tiles = [];
-  var ressources = new Map();
+  var manager = new ResourceManager();
+  var tiles = [];                      
+  var ressources = new Map();          
   
   /* Load tiles */
   for (var a = 1; a < 193; a++) {
@@ -27,10 +47,16 @@ void main() {
   ressources["map"] = new Json(manager, "map.json");
   ressources["transitions"] = new Json(manager, "transitions.json");
   
+  /* init game */
   var game = new Game(ressources);
+  
+  /* start game when all resources are loaded */
   manager.registerNotifyFunction(game.begin);
 }
 
+/**
+ * printf("%04d");
+ */
 String into4(var i) {
   if (i < 10)
     return "000$i";
